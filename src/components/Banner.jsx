a243bce2954a379d263b29b8948e23e369bd952a
Ridwan4D <ridwanul161@gmail.com>
2024-03-26 00:37:14 +0600

    banner section created

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,22 @@
-
 const Banner = () => {
-    return (
-        <div>
-            <h1 className="text-4xl">this is banner</h1>
-            
-        </div>
-    );
+  return (
+    <div className="p-8 md:p-24 bg-[#1313130D] rounded-3xl grid grid-cols-1 md:grid-cols-3 gap-y-5 text-center md:text-start">
+      <div className="col-span-2 md:p-10 space-y-5 md:space-y-10">
+        <h1 className="text-3xl md:text-6xl font-bold">
+          Books to freshen up your bookshelf
+        </h1>
+        <button
+          type="button"
+          className="px-2 py-1 md:px-5 md:py-2 text-sm md:text-lg font-semibold rounded bg-[#23BE0A] text-white"
+        >
+          View The List
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img src="/banner-img.png" alt="banner-img" className="w-full"/>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Banner from "../components/Banner";
 const Home = () => {
     return (
         <div>
-            <h1 className="text-4xl">this is home</h1>
             <Banner></Banner>
         </div>
     );
